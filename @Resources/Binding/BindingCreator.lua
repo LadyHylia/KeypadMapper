18a42afae9e56660259f6b2fd411e411f3e5f587
LadyHylia <95134341+LadyHylia@users.noreply.github.com>
2023-03-27 13:49:08 -0400

    working on binding creator script

--- @Resources/Binding/BindingCreator.lua
+++ @Resources/Binding/BindingCreator.lua
@@ -3,9 +3,36 @@ function Initialize()
 
     local current_profile = SKIN:GetVariable("CurrentProfile")
     
-
-    -- Specify the path and name of the output file
+	-- Specify the path and name of the output file
     local output_file = SKIN:MakePathAbsolute("Settings\Profiles\#CurrentProfile#\Bindings.ahk")
+	
+    -- Open the file for reading
+    local file = io.open(output_file, "r")
+
+    -- Read the contents of the file into a table
+    local lines = {}
+    for line in file:lines() do
+        table.insert(lines, line)
+    end
+
+    -- Modify the line you want to change
+    lines[3] = "This is the new content for line 3"
+
+    -- Close the file
+    file:close()
+
+    -- Reopen the file for writing
+    file = io.open(output_file, "w")
+
+    -- Write the modified contents back to the file
+    file:write(table.concat(lines, "\n"))
+
+    -- Close the file again
+    file:close()
+
+
+
+    
 
     -- Open the file in write mode
     local file = io.open(output_file, "w")
