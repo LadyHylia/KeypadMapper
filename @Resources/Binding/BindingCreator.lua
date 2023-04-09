dcd1af342b5efd952bd3c270552d7758897eef00
LadyHylia <LadyHylia1986@gmail.com>
2023-04-09 10:21:14 -0400

    working on BindingCreator.lua

--- @Resources/Binding/BindingCreator.lua
+++ @Resources/Binding/BindingCreator.lua
@@ -1,13 +1,137 @@
 -- Variables
-    Print("Hi!")
+    print("Hi!")
 -- Define a table
-local myTable = {"apple", "banana", "orange"}
+local myTable = {"", "banana", "orange"}
 
--- Define a value to check
-local myValue = "banana"
+local keyboard = {
+    ["escape"] = 1,
+    ["f1"] = 2,
+    ["f2"] = 3,
+    ["f3"] = 4,
+    ["f4"] = 5,
+    ["f5"] = 6,
+    ["f6"] = 7,
+    ["f7"] = 8,
+    ["f8"] = 9,
+    ["f9"] = 10,
+    ["f10"] = 11,
+    ["f11"] = 12,
+    ["f12"] = 13,
+    ["printscreen"] = 14,
+    ["scrolllock"] = 15,
+    ["pause"] = 16,
+    ["tilde"] = 17,
+    ["1"] = 18,
+    ["2"] = 19,
+    ["3"] = 20,
+    ["4"] = 21,
+    ["5"] = 22,
+    ["6"] = 23,
+    ["7"] = 24,
+    ["8"] = 25,
+    ["9"] = 26,
+    ["0"] = 27,
+    ["minus"] = 28,
+    ["equals"] = 29,
+    ["backspace"] = 30,
+    ["insert"] = 31,
+    ["home"] = 32,
+    ["pageup"] = 33,
+    ["numlock"] = 34,
+    ["numpaddivide"] = 35,
+    ["numpadmultiply"] = 36,
+    ["numpadminus"] = 37,
+    ["tab"] = 38,
+    ["q"] = 39,
+    ["w"] = 40,
+    ["e"] = 41,
+    ["r"] = 42,
+    ["t"] = 43,
+    ["y"] = 44,
+    ["u"] = 45,
+    ["i"] = 46,
+    ["o"] = 47,
+    ["p"] = 48,
+    ["delete"] = 52,
+    ["end"] = 53,
+    ["pagedown"] = 54,
+    ["numpadplus"] = 58,
+    ["capslock"] = 59,
+    ["a"] = 60,
+    ["s"] = 61,
+    ["d"] = 62,
+    ["f"] = 63,
+    ["g"] = 64,
+    ["h"] = 65,
+    ["j"] = 66,
+    ["k"] = 67,
+    ["l"] = 68,
+    ["semicolon"] = 69,
+    ["apostrophe"] = 70,
+    ["enter"] = 71,
+    ["leftshift"] = 75,
+    ["z"] = 76,
+    ["x"] = 77,
+    ["c"] = 78,
+    ["v"] = 79,
+    ["b"] = 80,
+    ["n"] = 81,
+    ["m"] = 82,
+    ["comma"] = 83,
+    ["period"] = 84,
+    ["slash"] = 85,
+    ["rightshift"] = 86,
+    [ "leftalt" ] = 86,
+    [ "space" ] = 87,
+    [ "rightalt" ] = 88,
+    [ "function" ] = 89,
+    [ "leftwin" ] = 90,
+    [ "rightwin" ] = 91,
+    [ "menu" ] = 92,
+    [ "rightctrl" ] = 93,
+    [ "numpad0" ] = 94,
+    [ "numpadperiod" ] = 95,
+    [ "numpadenter" ] = 96,
+    [ "numpad1" ] = 97,
+    [ "numpad2" ] = 98,
+    [ "numpad3" ] = 99,
+    [ "numpad4" ] = 100,
+    [ "numpad5" ] = 101,
+    [ "numpad6" ] = 102,
+    [ "numpad7" ] = 103,
+    [ "numpad8" ] = 104,
+    [ "numpad9" ] = 105,
+    [ "leftbracket" ] = 106,
+    [ "backslash" ] = 107,
+    [ "rightbracket" ] = 108,
+    [ "quote" ] = 109,
+    ["F13"] = 110,
+    ["F14"] = 111,
+    ["F15"] = 112,
+    ["F16"] = 113,
+    ["F17"] = 114,
+    ["F18"] = 115,
+    ["F19"] = 116,
+    ["F20"] = 117,
+    ["F21"] = 117,
+    ["F22"] = 119,
+    ["F23"] = 120,
+    ["F24"] = 121
+}
 
 -- create the tables
-local SpecialKeys = {}
+local SpecialKeys = {
+    ["escape"] = 1,
+    ["tab"] = 38,
+    ["capslock"] = 59,
+    [ "rightctrl" ] = 93,
+    [ "rightalt" ] = 88,
+    [ "function" ] = 89,
+    [ "leftwin" ] = 90,
+    [ "rightwin" ] = 91,
+}
+
+
 local Keys = {}
 local KeyTable = { SpecialKeys = SpecialKeys, Keys = Keys }
 
@@ -74,8 +198,16 @@ function Initialize()
 end
 
 
-function addKeyToTable(key, boolValue, targetTable)
+function addKeyToTable(CurrentKey, targetTable)
 
+-- Loop through the table and check if the value is present
+for _, value in ipairs(myTable) do
+    if value == myValue then
+        targetTable.SpecialKeys[Currentkey] = true
+      break
+    if 
+    end
+  end
 
     
 end
@@ -87,17 +219,11 @@ end
 
 
 
-Function DoTheThing(CurrentKey, IsSpecial, TargetTable)
+function DoTheThing(CurrentKey, IsSpecial, TargetTable)
     if IsSpecial then
-        targetTable.SpecialKeys[key] = true
+        targetTable.SpecialKeys[Currentkey] = true
     else
         targetTable.Keys[key] = true
     end
-End
--- Loop through the table and check if the value is present
-for _, value in ipairs(myTable) do
-  if value == myValue then
-    print("Value is present in table!")
-    break
-  end
 end
+
